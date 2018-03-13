b98f085702d9664f0c9d58582778aec663ada2c8
JiazaiWu <wujiazaiwh@163.com>
2018-03-13 06:50:35 -0700

    bresenham draw line why k (0,1) can handle the same with (-1, 0)

--- drawline/drawline.cpp
+++ drawline/drawline.cpp
@@ -5,14 +5,24 @@
 
 using namespace std;
 
+template<typename T>
+void swap_value(T& a, T& b) {
+	auto x = a;
+	a = b;
+	b = x;
+}
+
 int x_1 = 50;
-int y_1 = 400;
-int x_2 = 300; //y1 is declared??
+int y_1 = 300;
+int x_2 = 400; //y1 is declared??
 int y_2 = 50;
 
 void Init() {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glShadeModel(GL_FLAT);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glPointSize(1);
 }
 
 void Reshape(int w, int h)
@@ -31,10 +41,6 @@ void Reshape(int w, int h)
 
 void DDA()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0f, 1.0f, 1.0f);
-	glPointSize(1);
-
 	/*to draw*/
 	float stepLen = fabs(x_2-x_1) > fabs(y_2-y_1) ? fabs(x_2-x_1) : fabs(y_2-y_1);
 
@@ -62,10 +68,6 @@ void DDA()
 
 void compare()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0f, 1.0f, 1.0f);
-	glPointSize(1);
-
 	int nx, ny, offx, offy;
 	int F = 0;
 	int x = 0, y = 0;
@@ -115,12 +117,57 @@ void compare()
 }
 
 
+void bresenham()
+{
+	int dx = abs(x_2-x_1);
+	int dy = abs(y_2-y_1);
+	int flag = 0;
+
+	if (dy > dx) { //make k (-1, 1)
+		flag = 1;
+		swap_value(x_1, y_1);
+		swap_value(x_2, y_2);
+		swap_value(dx, dy);
+	}
+
+	int tx = (x_2 > x_1) ? 1:-1;
+	int ty = (y_2 > y_1) ? 1:-1;
+	int x = x_1;
+	int y = y_1;
+	int dS = 2*dy;
+	int dT = 2*(dy-dx);
+	int d = dS-dx; // first node's d
+
+	glBegin(GL_POINTS);
+	for (int i = 0; i < dx; i++) {
+		if(flag){
+			glVertex2i(y, x);
+		} else {
+			glVertex2i(x, y);
+		}
+
+		if(d < 0) {
+			d += dS;
+			x += tx;
+		}
+		else {
+			d +=dT;
+			y += ty;
+			x += tx;
+		}
+
+	}
+	glEnd();
+	glFlush();
+}
+
+
 int main(int argc,  char *argv[])
 {
 	void (*myDisplay)();
 
 	//myDisplay = DDA;
-	myDisplay = compare;
+	myDisplay = bresenham;
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
